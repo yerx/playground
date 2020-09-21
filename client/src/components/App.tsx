148c3b4b7d52aeb957d1050191b3a08e93c75d23
yerx <yer.x.xiong@gmail.com>
2020-09-21 14:54:09 -0400

    set up eslint with prettier and typescript

---
+++ client/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <main className="App">
+      <h1>let's do this</h1>
+    </main>
+  );
+};
+
+export default App;
